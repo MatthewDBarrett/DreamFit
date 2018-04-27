f0130464677bedebec42be4e00f45d4e88472f69
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2018-04-28 01:43:27 +1000

    Basic BMI Calculator functionality. Needs to round off decimals and a cleaner interface.

--- app/src/main/java/com/example/ethan/dream_fit/bmi_calculator.java
+++ app/src/main/java/com/example/ethan/dream_fit/bmi_calculator.java
@@ -2,6 +2,9 @@ package com.example.ethan.dream_fit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class bmi_calculator extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class bmi_calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
+    }
+
+    public void onCalculate(View view){
+        EditText kilogramText = (EditText) findViewById(R.id.kilogram);
+        EditText centimeterText = (EditText) findViewById(R.id.centimeter);
+        TextView resultText = (TextView) findViewById(R.id.resultText);
+        double kilogram, centimeter;
+        double bmiResult;
+
+        kilogram = Double.parseDouble(kilogramText.getText().toString());
+        centimeter = Double.parseDouble(centimeterText.getText().toString());
+        bmiResult = (kilogram/(centimeter/100))/(centimeter/100);
+
+        resultText.setText(String.valueOf(bmiResult));
     }
 }
