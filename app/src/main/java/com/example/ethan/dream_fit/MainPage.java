a1c93fa86ed1020ca1d0bf82aa3aa0cb7cf148de
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2018-05-05 16:09:37 +1000

    Changed the default icon of the profile.

--- app/src/main/java/com/example/ethan/dream_fit/MainPage.java
+++ app/src/main/java/com/example/ethan/dream_fit/MainPage.java
@@ -1,9 +1,16 @@
 package com.example.ethan.dream_fit;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainPage extends AppCompatActivity {
 
@@ -11,7 +18,9 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-
+        ImageButton profilePicture = (ImageButton) findViewById(R.id.profilePic);
+        profilePicture.setScaleType(ImageView.ScaleType.FIT_XY);
+        profilePicture.setAdjustViewBounds(true);
     }
 
     public void openStepCounter(View view){
--- app/src/main/java/com/example/ethan/dream_fit/profile_page.java
+++ app/src/main/java/com/example/ethan/dream_fit/profile_page.java
@@ -2,6 +2,7 @@ package com.example.ethan.dream_fit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class profile_page extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class profile_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        RecyclerView menuList;
     }
 }
