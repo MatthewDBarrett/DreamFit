69d5847847a3053b9a97ca2c92c55f690214e834
aviattri <37525378+aviattri@users.noreply.github.com>
2018-05-14 21:18:50 +1000

    Remove items's list implemented upto it's basic functionality

---
+++ app/src/main/java/com/example/ethan/dream_fit/calorie_tracker_remove_List.java
@@ -0,0 +1,20 @@
+package com.example.ethan.dream_fit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class calorie_tracker_remove_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_tracker_remove_list);
+
+        ListView mainListView = (ListView) findViewById(R.id.removeFood);
+        //mainListView
+    }
+
+
+
+}
