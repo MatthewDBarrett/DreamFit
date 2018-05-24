99ee003646cad6326ccf3d14808e4eadaf63e7cd
aviattri <37525378+aviattri@users.noreply.github.com>
2018-05-24 16:59:37 +1000

    Forgot to add OnTouchListener on seekbar

--- app/src/main/java/com/example/ethan/dream_fit/bmi_calculator.java
+++ app/src/main/java/com/example/ethan/dream_fit/bmi_calculator.java
@@ -2,6 +2,7 @@ package com.example.ethan.dream_fit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
@@ -18,6 +19,12 @@ public class bmi_calculator extends AppCompatActivity {
         setContentView(R.layout.activity_bmi_calculator);
         progressBar = findViewById(R.id.seekBarBMI);
         progressBar.setProgress(0);
+        progressBar.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
     }
 
     public void onCalculate(View view){
