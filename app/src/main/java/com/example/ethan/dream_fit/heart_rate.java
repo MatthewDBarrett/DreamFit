f2efc9aabf5d4384d9b4f7dd885c49fc53fe16ef
Matt's-PC <matthew.barrett@aaacomms.com>
2018-05-24 18:59:15 +1000

    Implemented heart rate functionality through Sensor API

--- app/src/main/java/com/example/ethan/dream_fit/heart_rate.java
+++ app/src/main/java/com/example/ethan/dream_fit/heart_rate.java
@@ -1,13 +1,60 @@
 package com.example.ethan.dream_fit;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class heart_rate extends AppCompatActivity {
+public class heart_rate extends AppCompatActivity implements SensorEventListener {
+
+    TextView tv_heart_rate;
+
+    SensorManager sensorManager;
+
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate);
+
+        tv_heart_rate = (TextView) findViewById(R.id.tv_heart_rate);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        if(countSensor != null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(running){
+            tv_heart_rate.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
