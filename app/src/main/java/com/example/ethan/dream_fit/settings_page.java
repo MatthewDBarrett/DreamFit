447711922fc6be2bff636b7f872b3cd6fede8a3d
MohamadWin <mohwin1997@hotmail.com>
2018-05-28 21:29:17 +1000

    FOR TUTOR: This was comitted but not pushed by due date. After asking lecturer, he said I am permitted to push tonight as long as i make a comment about it.

--- app/src/main/java/com/example/ethan/dream_fit/settings_page.java
+++ app/src/main/java/com/example/ethan/dream_fit/settings_page.java
@@ -10,19 +10,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 public class settings_page extends AppCompatActivity {
 
     DatabaseHelper myDB;
     DatabaseHelper_Main main_DB;
     SharedPreferences prefs;
     final Context context = this;
-
+    private static final String EMAIL = "email";
+    public static CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+        callbackManager = CallbackManager.Factory.create();
         prefs = context.getSharedPreferences(
                 "com.example.ethan.dream_fit", Context.MODE_PRIVATE);
+
+        LoginButton loginButton;
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 
     public void onEmpty(View view){
